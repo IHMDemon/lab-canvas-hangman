8fc4d75b322ca9aef432a668d85d176f8a18343c
Zeta <ihmdemon.exe@gmail.com>
2018-06-22 16:58:53 -0400

    Revisied working version of hangman

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,37 @@
-var hangman;
-
-// function Hangman() {
-
-// }
-
-// Hangman.prototype.getWord = function () {
-
-// };
-
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+var theGame, theGameCanvas;
+function Hangman() {
+this.words = ['dog', 'cat', 'fish', 'parrot', 'hyena', 'lion', 'rhinocerous'];
+this.secretWord = "";
+this.badGuesses = [];
+this.goodGuesses = [];
+this.errorsLeft = 10;
+this.wordSoFar = [];
+}
+Hangman.prototype.getWord = function () {
+  this.secretWord = this.words[Math.floor(Math.random()*this.words.length)];
 };
-
-
-document.onkeydown = function (e) {
-
-};
+Hangman.prototype.checkLetter = function (letter) {
+  var result;
+  if(!letter.match(/[a-z]/)){
+    alert('sorry, that is not a letter. please try a letter instead.')
+    return;
+  }
+  if(this.badGuesses.includes(letter) || this.goodGuesses.includes(letter)){
+    alert ("you've already guessed that letter please try another");
+    return;
+  }
+  var that = this;
+  if (this.secretWord.includes(letter)){
+    this.goodGuesses.push(letter);
+    result = true;
+  } else{
+    this.badGuesses.push(letter);
+    result = false;
+    this.errorsLeft -=1;
+  }
+  this.secretWord.split('').forEach(function(eachLetter, index){
+    if(eachLetter ===  letter ){
+      that.wordSoFar[index] = letter;
+    }
+  });
+  console.log(this);
